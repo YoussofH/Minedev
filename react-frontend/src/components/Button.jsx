de0cbebc32c771f68303407847efbbeb12375d41
Youssof Hammoud <youssofhammoud@gmail.com>
2024-05-01 15:57:46 +0300

    applied color blue on buttons

--- react-frontend/src/components/Button.jsx
+++ react-frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ variant = 'full', children, ...otherProps }) => {
   const buttonClasses = {
-    full: 'bg-neutral-700 hover:bg-neutral-600 rounded-lg py-2 px-4 border-b-4 border-neutral-800 text-white',
+    full: 'bg-sky-600 hover:bg-sky-500 rounded-lg py-2 px-4 border-b-4 border-sky-800 text-white',
     light: 'font-semibold px-3 backdrop:blur-md hover:backdrop:blur-lg py-2 hover:transparent text-black hover:bg-slate-100 ease-in-out rounded-md transition-all cursor-pointer',
   };
 
