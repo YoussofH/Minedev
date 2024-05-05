8b0307c1bf2d2980f19c31720f3536f230a063ed
Youssof Hammoud <youssofhammoud@gmail.com>
2024-05-05 16:42:02 +0300

    Backend <> registered models in admin dashboard

--- WebApp/django-docker-backend/main_app/admin.py
+++ WebApp/django-docker-backend/main_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import (Plan, Project, Feature, BotResponse, Conversation,
+                     Developer, DeveloperPrompt, Enterprise, EnterpriseDeveloper)
 
 # Register your models here.
+admin.site.register(Plan)
+admin.site.register(Project)
+admin.site.register(Feature)
+admin.site.register(BotResponse)
+admin.site.register(Conversation)
+admin.site.register(Developer)
+admin.site.register(DeveloperPrompt)
+admin.site.register(Enterprise)
+admin.site.register(EnterpriseDeveloper)
