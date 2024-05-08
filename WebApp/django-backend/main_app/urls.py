380289e164827fc40cdf83bceea9430269a88585
Youssof Hammoud <youssofhammoud@gmail.com>
2024-05-09 00:01:11 +0300

    Backend <> fixed typo

--- WebApp/django-backend/main_app/urls.py
+++ WebApp/django-backend/main_app/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import PlanViewSet
 
-from .views import UserListAPiCreateView
+from .views import UserProfileListCreateView
 from .views import CustomTokenObtainPairView
 
 router = DefaultRouter()
@@ -11,6 +11,6 @@ router.register('plans', PlanViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('user/register/', UserListAPiCreateView.as_view(), name='register'),
+    path('user/register/', UserProfileListCreateView.as_view(), name='register'),
     path('user/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair')
 ]
