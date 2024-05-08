964d30b6d3d2b35bd15abd9cf580a0742addbe1e
Youssof Hammoud <youssofhammoud@gmail.com>
2024-05-08 23:09:08 +0300

    Django Views <> created DRF ListCreateView for "Developer" user

--- WebApp/django-backend/main_app/urls.py
+++ WebApp/django-backend/main_app/urls.py
@@ -3,9 +3,14 @@ from rest_framework.routers import DefaultRouter
 
 from .views import PlanViewSet
 
+from .views import UserListAPiCreateView
+from .views import CustomTokenObtainPairView
+
 router = DefaultRouter()
 router.register('plans', PlanViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('user/register/', UserListAPiCreateView.as_view(), name='register'),
+    path('user/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair')
 ]
--- WebApp/django-backend/main_app/views.py
+++ WebApp/django-backend/main_app/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from .models import Plan
+from .models import Plan, Developer
 from .serializers import PlanSerializer
 from rest_framework.viewsets import ModelViewSet
 
 class PlanViewSet(ModelViewSet):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
+
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import UserModelSerializer
+
+class UserProfileListCreateView(ListCreateAPIView):
+    """Generic View for Listing and Creating User Profiles"""
+
+    queryset = Developer.objects.all()
+    serializer_class = UserModelSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, validated_data):
+        user = Developer.objects.create_user(**validated_data)
+        return user
