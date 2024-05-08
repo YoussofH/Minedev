51a2bc09fbd3638d0c3e9c4354081d890132e7cd
Youssof Hammoud <youssofhammoud@gmail.com>
2024-05-08 23:06:11 +0300

    Django <> created custom user model"Developer"

--- WebApp/django-backend/main_app/models.py
+++ WebApp/django-backend/main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
 class Plan(models.Model):
     name  = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
@@ -8,6 +8,40 @@ class Plan(models.Model):
     def __str__(self):
         return self.name
     
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+# Custom User
+class Developer(AbstractBaseUser, PermissionsMixin):
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    plan = models.ForeignKey(Plan, on_delete= models.CASCADE)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
 class Feature(models.Model):
     chatbot = models.BooleanField()
     navigation = models.BooleanField()
@@ -21,15 +55,6 @@ class Feature(models.Model):
             "AI Talk" if self.ai_talk else ""
         ]
         return f"Feature: {', '.join(features)}"
-
-class Developer(models.Model):
-    fullname = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=255)
-    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.fullname
 
 class Enterprise(models.Model):
     name =  models.CharField(max_length=100)
