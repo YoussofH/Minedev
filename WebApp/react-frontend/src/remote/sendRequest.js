1928feaf0ade7e9243506822772cba5cbe2d2c20
Youssof Hammoud <youssofhammoud@gmail.com>
2024-05-10 16:06:07 +0300

    API <> remove appended slash for the api base URL

--- WebApp/react-frontend/src/remote/sendRequest.js
+++ WebApp/react-frontend/src/remote/sendRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const sendRequest = async (method, route, body) => {
     try {
-        axios.defaults.baseURL = "http://18.219.38.17:8000/api/";
+        axios.defaults.baseURL = "http://18.219.38.17:8000/api";
 
         const response = await axios.request({
             method: method,
