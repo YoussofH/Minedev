c44dfc32ad233ddc58df63eb6c03cc81decc3353
Youssof Hammoud <youssofhammoud@gmail.com>
2024-05-10 16:01:09 +0300

    React <> created logout button style

--- WebApp/react-frontend/src/components/Button.jsx
+++ WebApp/react-frontend/src/components/Button.jsx
@@ -4,6 +4,7 @@ const Button = ({ variant = 'full', children, ...otherProps }) => {
   const buttonClasses = {
     full: 'bg-sky-600 hover:bg-sky-500 rounded-lg py-2 px-4 border-b-4 border-sky-800 text-white',
     light: 'font-semibold px-3 backdrop:blur-md hover:backdrop:blur-lg py-2 hover:transparent text-black hover:bg-slate-100 ease-in-out rounded-md transition-all cursor-pointer',
+    logout: 'font-semibold px-3 backdrop:blur-md hover:backdrop:blur-lg py-1 hover:transparent text-red-500 border-red-500 border-2 hover:bg-red-100 ease-in-out rounded-md transition-all cursor-pointer',
   };
 
   return (
