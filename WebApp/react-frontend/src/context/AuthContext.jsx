11b98280ec94f2f68fa139824ae107adf4097a59
Youssof Hammoud <youssofhammoud@gmail.com>
2024-05-10 16:05:07 +0300

    React <> fixed auth token saving functionality, and adjusted isAuthenticated state

--- WebApp/react-frontend/src/context/AuthContext.jsx
+++ WebApp/react-frontend/src/context/AuthContext.jsx
@@ -1,6 +1,5 @@
-import { createContext, useState } from 'react'
-import jwtDecode from 'jwt-decode';
-import { useNavigate } from 'react-router-dom'
+import { createContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import sendRequest from '../remote/sendRequest';
 
 const AuthContext = createContext()
@@ -9,38 +8,36 @@ export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
 
-    let [user, setUser] = useState(null)
-    let [authTokens, setAuthTokens] = useState(null)
+    let [isAuthenticated, setIsAuthenticated] = useState(() => (localStorage.getItem('authTokens') ? true : false))
+    let [authTokens, setAuthTokens] = useState(() => (localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null))
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     let loginUser = async (e) => {
         e.preventDefault()
 
-        const response = await sendRequest("POST", "user/token/", { username: e.target.username.value, password: e.target.password.value })
-        let data = await response.json();
-
-        if (data) {
-            localStorage.setItem('authTokens', JSON.stringify(data));
-            setAuthTokens(data)
-            setUser(jwtDecode(data.access))
-            navigate('/')
-        } else {
-            alert('Something went wrong while loggin in the user!')
-        }
+        await sendRequest("POST", "/user/token/", { email: e.target.email.value, password: e.target.password.value })
+            .then((response) => {
+                    localStorage.setItem('authTokens', JSON.stringify(response.data));
+                    setAuthTokens(response.data);
+                    setIsAuthenticated(true);
+                    navigate('/');
+            }).catch((error) => {
+                console.log("Something went wrong with login");
+                console.log(error);
+            });
     }
 
     let logoutUser = (e) => {
-        e.preventDefault()
-        localStorage.removeItem('authTokens')
-        setAuthTokens(null)
-        setUser(null)
-        navigate('/login')
-
+        e.preventDefault();
+        localStorage.removeItem('authTokens');
+        setAuthTokens(null);
+        setIsAuthenticated(false);
+        navigate('/');
     }
 
     let contextData = {
-        user: user,
+        isAuthenticated: isAuthenticated,
         authTokens: authTokens,
         loginUser: loginUser,
         logoutUser: logoutUser,
