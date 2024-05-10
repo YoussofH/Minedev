781413f4411334ba9eb84a730885647e6b2ddb6a
YoussofH <youssofhammoud@gmail.com>
2024-05-10 18:21:42 +0000

    Django <> allowed cors origins

--- WebApp/django-backend/core/settings.py
+++ WebApp/django-backend/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'main_app',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,3 +149,5 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = 'main_app.Developer'
+
+CORS_ALLOW_ALL_ORIGINS = True
