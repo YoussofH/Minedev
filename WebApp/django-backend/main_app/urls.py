50b6816bd644d7e6429d80bc05110a2f7053b309
YoussofH <youssofhammoud@gmail.com>
2024-05-11 12:11:58 +0000

    Django <> created bot response and user prompt views

--- WebApp/django-backend/main_app/urls.py
+++ WebApp/django-backend/main_app/urls.py
@@ -4,14 +4,16 @@ from rest_framework.routers import DefaultRouter
 from .views import PlanViewSet
 
 from .views import CustomTokenObtainPairView, UserProfileListCreateView
-from .views import get_profile
+from .views import get_profile, DeveloperPromptListCreateView, BotResponseListViewSet
 
 router = DefaultRouter()
 router.register('plans', PlanViewSet)
+router.register('bot/responses', BotResponseListViewSet, basename='bot-responses')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('user/register/', UserProfileListCreateView.as_view(), name='register'),
     path('user/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('user/profile/', get_profile, name='profile')
+    path('user/profile/', get_profile, name='profile'),
+    path('user/prompt/', DeveloperPromptListCreateView.as_view(), name='prompt'),
 ]
--- WebApp/django-backend/main_app/views.py
+++ WebApp/django-backend/main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Plan, Developer
-from .serializers import PlanSerializer, DeveloperModelSerializer, CustomTokenObtainPairSerializer
+from .models import Plan, Developer, DeveloperPrompt, BotResponse, Conversation
+from .serializers import PlanSerializer, DeveloperModelSerializer, CustomTokenObtainPairSerializer, DeveloperPromptSerializer, BotResponseSerializer
 
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
@@ -8,15 +8,68 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework import viewsets
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_profile(request):
     user = request.user
     serializer = DeveloperModelSerializer(user, many=False)
-    return Response(serializer.data)
+    return Response(status=status.HTTP_201_CREATED)
+
+
+class BotResponseListViewSet(ModelViewSet):
+    """ViewSet for Listing Bot Responses"""
+    permission_classes = [IsAuthenticated]
+
+    queryset = BotResponse.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        conversation = self.request.query_params.get('conversation', None)
+
+        if conversation is not None:
+            # Check ownership using filter and exists
+            is_owner = Conversation.objects.filter(pk=conversation, user=user).exists()
+
+            if is_owner:
+                return self.queryset.filter(conversation=conversation)
+            else:
+                return BotResponse.objects.none()
+        else:
+            raise serializers.ValidationError("Conversation parameter is required")
+
+
+    def get_serializer_class(self):
+        """Returns the serializer class based on the request method."""
+        if self.request.method == 'GET':
+            return BotResponseSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class DeveloperPromptListCreateView(ListCreateAPIView):
+    """Generic View for Listing and Creating Developer Prompts"""
+
+    serializer_class = DeveloperPromptSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        conversation = self.request.query_params.get('conversation', None)
+        return DeveloperPrompt.objects.filter(user=user, conversation__project=conversation)
+
+    def create(self, request, *args, **kwargs):
+        user = self.request.user
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class PlanViewSet(ModelViewSet):
