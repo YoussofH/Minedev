0b80e7f033c8ed5042019367adcea8b1e7f16b0b
YoussofH <youssofhammoud@gmail.com>
2024-05-13 23:15:56 +0000

    Websocket <> automatic channel bot response

--- WebApp/django-backend/main_app/consumers.py
+++ WebApp/django-backend/main_app/consumers.py
@@ -1,21 +1,49 @@
 import json
-
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 
 import asyncio
 from ollama import AsyncClient
 
+from .models import BotResponse, Conversation
 
-class ChatConsumer(WebsocketConsumer):    
-    def connect(self):
-        self.accept()
-        self.send(text_data=json.dumps({"message": "welcome here"}))
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.send(text_data=json.dumps({"message": "Welcome here!"}))
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         pass
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        message = text_data_json["user_prompt"]
 
-        self.send(text_data=json.dumps({"message": message}))
+        
+
+        end_result = ""
+
+        async def get_response():
+            msg = {'role': 'user', 'content': message}
+            response = await AsyncClient(host='http://3.16.245.247:11434').chat(model='llama3:8b', messages=[msg], stream=True)
+            return response
+
+        async def send_chunks(response, end_result=""):
+            async for part in response:
+                res = part['message']['content']
+                end_result += res
+                await self.send(text_data=json.dumps({"message": res}))
+            return end_result
+
+        response = await get_response()
+        end_result = await send_chunks(response)
+
+        @database_sync_to_async
+        def create_bot_response(conversation_id, end_result):
+            conversation = Conversation.objects.get(pk=conversation_id)
+            return BotResponse.objects.create(conversation=conversation, history_context="", content=end_result)
+        
+        conversation_id = int(self.scope['url_route']['kwargs']['room_name'])
+        await create_bot_response(conversation_id, end_result)
+
+        await self.send(text_data=json.dumps({"message": "done!!!!!!!!!!!!!"}))
