e6ff7dde83029a093749ef26de52df033e12d6fb
YoussofH <youssofhammoud@gmail.com>
2024-05-13 15:50:34 +0000

    Django <> simple working websocket

--- WebApp/django-backend/main_app/consumers.py
+++ WebApp/django-backend/main_app/consumers.py
@@ -1,40 +1,21 @@
 import json
 
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 
 import asyncio
 from ollama import AsyncClient
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.client =  AsyncClient(host='http://3.16.245.247:11434')
-    
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):    
+    def connect(self):
         self.accept()
-        await self.send(text_data=json.dumps({"message": "welcome here"}))
+        self.send(text_data=json.dumps({"message": "welcome here"}))
 
-    async def disconnect(self, close_code):
+    def disconnect(self, close_code):
         pass
 
-    async def receive(self, text_data):
-        '''text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        self.send(text_data=json.dumps({"message": message}))'''
-
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        # Use your chatbot library (Ollama in this case) to process the message and get responses
-        async def get_response():
-            response = await self.client.chat(model='llama3:8b', messages=[{"role": "user", "content": message}], stream=True)
-            return response
-
-        async def send_chunks(response):
-            async for part in response:
-                await self.send(text_data=json.dumps({"message": part['message']['content']}))
-
-        loop = asyncio.new_event_loop()
-        loop.run_until_complete(send_chunks(loop.run_until_complete(get_response())))
+        self.send(text_data=json.dumps({"message": message}))
