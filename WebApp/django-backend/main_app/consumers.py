790a82867ac6df29cf00739a0cf790fe091c9993
YoussofH <youssofhammoud@gmail.com>
2024-05-18 15:08:52 +0000

    Backend <> added hierarchy context for chat

--- WebApp/django-backend/main_app/consumers.py
+++ WebApp/django-backend/main_app/consumers.py
@@ -21,7 +21,7 @@ ollama_client = AsyncClient(host='http://3.16.245.247:11434')
 
 #os.environ['hierarchy_context'] = ""
 #os.environ['project_context'] = ""
-os.environ['workspace_rules_prompt'] =   """RULES: YOU ARE AN AI PAIR PROGRAMMER THAT ASSISTS THE DEVELOPER BY STRUCTURING IDEAS AND GIVING THEM CONCISE ANSWERS WITHOUT UNNEEDED INTRODUCTIONS. YOU ARE GOING TO BE GIVEN HIERARCHY OF THE USER'S WORKSPACE, YOU ARE GOING TO ANALYZE IT THOROUGHLY. IN SOME OF YOUR RESPONSES YOU MIGHT HAVE TO TELL THE USER TO NAVIGATE TO A SPECIFIC FILE ACCORDING TO THE HIERARCHY, SO THAT YOU WILL HAVE TO RESPOND USING STRICTLY THE FOLLOWING FORMAT HTML TAG <a href="FILE PATH HERE">FILE NAME HERE</a>. NOTE: THE FILE PATH SHOULD ONLY BE RELATED TO THE HIERARCHY AND NOTHING ELSE. YOU ARE NOT ALLOWED TO GIVE USER COMPLETE CODE. ABIDE BY THE RULES."""
+os.environ['workspace_rules_prompt'] =   """RULES: YOU ARE AN AI PAIR PROGRAMMER THAT ASSISTS ME THE DEVELOPER BY STRUCTURING IDEAS AND GIVING THEM CONCISE ANSWERS WITHOUT UNNEEDED INTRODUCTIONS. YOU ARE GOING TO BE GIVEN HIERARCHY OF THE USER'S WORKSPACE, YOU ARE GOING TO ANALYZE IT THOROUGHLY. IN SOME OF YOUR RESPONSES YOU MIGHT HAVE TO TELL THE USER TO NAVIGATE TO A SPECIFIC FILE ACCORDING TO THE HIERARCHY, SO THAT YOU WILL HAVE TO RESPOND USING STRICTLY THE FOLLOWING FORMAT HTML TAG <a href="FILE PATH HERE">FILE NAME HERE</a>. NOTE: THE FILE PATH SHOULD ONLY BE RELATED TO THE HIERARCHY AND NOTHING ELSE. YOU ARE NOT ALLOWED TO GIVE USER COMPLETE CODE. ABIDE BY THE RULES."""
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -37,9 +37,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json["user_prompt"]
 
         @database_sync_to_async
-        def get_conversation_content(conversation_id=3):
+        def get_conversation_content(conversation_id):
             prompts = DeveloperPrompt.objects.filter(conversation_id=conversation_id).order_by('sent_at')
             responses = BotResponse.objects.filter(conversation_id=conversation_id).order_by('sent_at')
+            hierarchy = Conversation.objects.get(pk=conversation_id).hierarchy
             #can i just extract content field for all prompts and all responses then join them together using normal python
 
             messages = [message.content for message in DeveloperPrompt.objects.filter(conversation_id=conversation_id).order_by('sent_at')]
@@ -48,13 +49,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
             # Join content with newline for readability
             content = "\n".join(messages)
 
-            return content.strip()
+            return (content.strip(), hierarchy)
 
 
         conversation_id = int(self.scope['url_route']['kwargs']['room_name'])
-        chat_context = await get_conversation_content(conversation_id)
+        chat_context, hierarchy = await get_conversation_content(conversation_id)
         await self.send(text_data=json.dumps({"chat_context": chat_context}))
-        message = f"{os.environ['workspace_rules_prompt']}. HERE IS CHAT HISTORY BETWEEN YOU AND THE USER:{chat_context}. RESPOND ACCORDING TO THE RULES. HERE IS THE USER'S REQUEST: {message}"
+        message = f"{os.environ['workspace_rules_prompt']}. HERE IS CHAT HISTORY BETWEEN YOU AND THE USER:{chat_context}. HERE IS THE WORKSPACE HIERARCHY: {hierarchy}. RESPOND ACCORDING TO THE RULES. HERE IS THE USER'S REQUEST: {message}"
 
         end_result = ""
 
@@ -139,7 +140,14 @@ class ProjectTitleConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         workspaceTree = text_data_json["workspaceTree"]
-        #os.environ['hierarchy_context'] = workspaceTree
+        conversation_id = text_data_json["conversationID"]
+
+        @database_sync_to_async
+        def add_hierarchy(conversation_id, workspaceTree):
+            conversation = Conversation.objects.get(pk=conversation_id)
+            conversation.hierarchy = workspaceTree
+            return conversation.save()
+        await add_hierarchy(conversation_id, workspaceTree)
         
         end_result = ""
         prompt = f"HERE IS MY WORKSPACE TREE PLEASE GENERATE A SUITABLE TITLE FOR MY PROJECT. YOU ARE STRICTLY PROMPTED TO JUST GIVE AN ANSWER WITH THE TITLE ONLY, NO INTRODUCTIONS NOTHING ELSE IS ALLOWED. JUST RETURN PROJECT TITLE, DO NOT USE SAME TITLE AS THE MAIN FOLDER, YOUR ANSWER SHOULD CONTAIN ONLY ENGLISH CHARACTERS NO QUOTES OR OTHER SYMBOLS, YOU ARE ALLOWED TO ADD SPACES BETWEEN WORDS. IMPORTANT NOTE: TITLE SHOULD BE STRICTLY NO LONGER THAN 3 WORDS: {workspaceTree}"
